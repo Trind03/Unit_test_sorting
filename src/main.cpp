13700ac114393381c0f13b2c98fbb45b5a45af06
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-03 21:00:12 +0100

    Added to cmake file and delected cout in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     
     std::vector<int>(array) = {9,8,7,6,5,4,3,2,1};
-    //std::cout << cocktailSort(array); render<int>(array);
+    
     render(array);
     return EXIT_SUCCESS;
 }
