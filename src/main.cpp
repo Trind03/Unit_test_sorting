56990f8676ce979778903ea7dfab34858d07594f
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-04 18:27:13 +0100

    Added message to the render function

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@ TEST(Unit_cocktailSort,cocktailSort)
 {
     std::vector<int>(correct) = {1,2,3,4,5,6,7,8,9};
     std::vector<int>(nums) = {4,7,2,9,5,3,1,6,8};
-    //render(correct);   
+
+    
+    render("Predefined array -> ",correct);   
     cocktailSort(nums);
-    //render(nums);  
+    render("Sorted array     -> ",nums);  
     EXPECT_EQ(correct,nums);
 }
 
