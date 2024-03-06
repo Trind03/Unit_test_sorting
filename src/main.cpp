e0e9a1db0af4a129d81fe05c630e9beb4b24af7f
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-06 10:04:37 +0100

    Update main.cpp
    Added messages to the testcase and a function to render the vector with a message.

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,11 @@ TEST(Unit_bubbleSort,bubbleSort)
 {
     std::vector<int>(correct) = {1,2,3,4,5,6,7,8,9};
     std::vector<int>(nums) = {4,7,2,9,5,3,1,6,8};
-    std::cout << "Bouble Sort\n";
 
+    std::cout << "Bouble Sort\n";
+    render("Predefined array -> ",correct);   
     bubbleSort(nums);
+    render("Sorted array     -> ",nums); 
  
     EXPECT_EQ(correct,nums);
 }
