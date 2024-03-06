5f60dcf8100fbdd6b7de34d6a81ae3519d92a69b
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-06 10:01:25 +0100

    Added an expect_eq()

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,8 @@ TEST(Unit_bubbleSort,bubbleSort)
     std::vector<int>(correct) = {1,2,3,4,5,6,7,8,9};
     std::vector<int>(nums) = {4,7,2,9,5,3,1,6,8};
 
-
+ 
+    EXPECT_EQ(correct,nums);
 }
 
 int main(int argc, char *argv[])
