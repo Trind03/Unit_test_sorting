b74a84f19713cfe4425cb9bda4e06ea8bd06fbbb
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-06 09:58:57 +0100

    New test
    Added new testcase and declared two vectors.

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,16 @@ TEST(Unit_cocktailSort,cocktailSort)
     EXPECT_EQ(correct,nums);
 }
 
+TEST(Unit_bubbleSort,bubbleSort)
+{
+    std::vector<int>(correct) = {1,2,3,4,5,6,7,8,9};
+    std::vector<int>(nums) = {4,7,2,9,5,3,1,6,8};
 
 
+}
 
 int main(int argc, char *argv[])
 {
    testing::InitGoogleTest(&argc,argv);
    return RUN_ALL_TESTS();
-
 }
