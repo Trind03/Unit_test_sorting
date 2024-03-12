9f24697e9b72d062b189ea1c56876fa3bae4a02b
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-12 11:02:21 +0100

    Refactoring code

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,20 @@
 #include "bubbleSort.h"
 #include "selectionSort.h"
 
-std::ostream& operator<<(std::ostream &stream, std::array<int,10>&array)
+std::ostream& operator<<(std::ostream &stream, std::array<int,10>& array)
 {
     stream << "\n";
     for(int arr : array) stream << arr << " - ";
     stream << "\n";
-    return stream;
+    
+    return (stream);
 }
 
 TEST(Unit_cocktailSort,cocktailSort)
 {
     Cocktailsort cocktailsort({0,1,2,3,4,5,6,7,8,9}); // {4,7,2,9,5,3,1,6,8,0}
     cocktailsort.sort({4,7,2,9,5,3,1,6,8,0});
+    
     std::cout << cocktailsort.generated;
     EXPECT_EQ(cocktailsort.correct,cocktailsort.generated);
 }
