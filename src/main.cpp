0f2da75eac8e8a631753bdb84877f42f47de25eb
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-12 18:59:12 +0100

    Removed comment

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ std::ostream& operator<<(std::ostream &stream, std::array<int,10>& array)
 
 TEST(Unit_cocktailSort,cocktailSort)
 {
-    Cocktailsort cocktailsort({0,1,2,3,4,5,6,7,8,9}); // {4,7,2,9,5,3,1,6,8,0}
+    Cocktailsort cocktailsort({0,1,2,3,4,5,6,7,8,9});
     cocktailsort.sort({4,7,2,9,5,3,1,6,8,0});
     
     std::cout << cocktailsort.generated;
