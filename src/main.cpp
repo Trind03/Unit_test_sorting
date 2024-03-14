3911b9bfdfd3a043133a7cca8853f788cba9bf89
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-14 13:49:49 +0100

    Moved commented out overload

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,6 @@
 #include <gtest/gtest.h>
 #include "tests.h"
 
-
-int main(int argc, char *argv[])
-{
-    testing::InitGoogleTest(&argc,argv);
-    std::future<int>process = std::async(std::launch::async,RUN_ALL_TESTS);
-    return process.get();
-}
-
 /* Overload for << to print out forted array
 
 std::ostream& operator<<(std::ostream &stream, std::vector<int>& array)
@@ -24,4 +16,12 @@ std::ostream& operator<<(std::ostream &stream, std::vector<int>& array)
     return stream;
 }
 */
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc,argv);
+    std::future<int>process = std::async(std::launch::async,RUN_ALL_TESTS);
+    return process.get();
+}
+
 
