c1b75142205b5f43893d0e4e6555a7075a5a0856
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-15 10:21:46 +0100

    Moved bitshift left overload

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,6 @@
 #include <gtest/gtest.h>
 #include "tests.h"
 
-#define __Overload__
-/* Overload for << to print out forted array*/
-
-#ifdef __Overload__
-std::ostream& operator<<(std::ostream &stream, std::vector<int>& array)
-{
-    stream << "\n";
-    for(int arr : array) stream << arr << " - ";
-    stream << "\n";
-
-    return stream;
-}
-#endif
-
 int main(int argc, char *argv[])
 {
     testing::InitGoogleTest(&argc,argv);
