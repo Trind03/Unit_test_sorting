98a8cd86e55c8a375ab933fd04d4c31d207d5ed6
Torstein <141224356+Trind03@users.noreply.github.com>
2024-03-23 23:58:50 +0100

    Update
    Added support for mergeSort and, not a stable release

--- src/algo/mergeSort.h
+++ src/algo/mergeSort.h
@@ -2,73 +2,9 @@
 #include <array>
 #include "algorithm.h"
 
-class MergeSort : public algorithm
+class MergeSort : private algorithm
 {
     MergeSort(std::array<int,arraySize>&correct,std::array<int,arraySize>&generated);
-    void mergeSort(int array[], int const begin, int const end);
-    void merge(int array[], int const left, int const mid,int const right);
+    void merge(std::array<int,arraySize>&array, int const left, int const mid,int const right);
+    void mergeSort(std::array<int,arraySize>&array, int const begin, int const end);
 };
-
-/*
-void merge(int array[], int const left, int const mid,
-           int const right)
-{
-    int const subArrayOne = mid - left + 1;
-    int const subArrayTwo = right - mid;
- 
-    auto *leftArray = new int[subArrayOne],
-         *rightArray = new int[subArrayTwo];
- 
-    for (auto i = 0; i < subArrayOne; i++)
-        leftArray[i] = array[left + i];
-    for (auto j = 0; j < subArrayTwo; j++)
-        rightArray[j] = array[mid + 1 + j];
- 
-    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
-    int indexOfMergedArray = left;
- 
-    while (indexOfSubArrayOne < subArrayOne
-           && indexOfSubArrayTwo < subArrayTwo) {
-        if (leftArray[indexOfSubArrayOne]
-            <= rightArray[indexOfSubArrayTwo]) {
-            array[indexOfMergedArray]
-                = leftArray[indexOfSubArrayOne];
-            indexOfSubArrayOne++;
-        }
-        else {
-            array[indexOfMergedArray]
-                = rightArray[indexOfSubArrayTwo];
-            indexOfSubArrayTwo++;
-        }
-        indexOfMergedArray++;
-    }
-
-    while (indexOfSubArrayOne < subArrayOne) {
-        array[indexOfMergedArray]
-            = leftArray[indexOfSubArrayOne];
-        indexOfSubArrayOne++;
-        indexOfMergedArray++;
-    }
- 
-
-    while (indexOfSubArrayTwo < subArrayTwo) {
-        array[indexOfMergedArray]
-            = rightArray[indexOfSubArrayTwo];
-        indexOfSubArrayTwo++;
-        indexOfMergedArray++;
-    }
-    delete[] leftArray;
-    delete[] rightArray;
-}
- 
-
-void mergeSort(int array[], int const begin, int const end)
-{
-    if (begin >= end)
-        return;
- 
-    int mid = begin + (end - begin) / 2;
-    mergeSort(array, begin, mid);
-    mergeSort(array, mid + 1, end);
-    merge(array, begin, mid, end);
-}*/
