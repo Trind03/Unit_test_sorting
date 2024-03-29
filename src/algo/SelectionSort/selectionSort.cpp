ea0b13ff9c84bc79ffb3510c362169bf14c9fa27
Torstein <torsrind@icloud.com>
2024-03-30 00:15:42 +0100

    Minor refactoring

--- src/algo/SelectionSort/selectionSort.cpp
+++ src/algo/SelectionSort/selectionSort.cpp
@@ -5,18 +5,17 @@
 
 
 SelectionSort::SelectionSort(std::array<int,arraySize>&correct,std::array<int,arraySize>&generated) : Correct(correct), Generated(generated)
-{
-	
-};
+{};
 
 void SelectionSort::sort(std::array<int,arraySize>&arr) 
 { 
 	int i, j, min_idx; 
-	for (i = 0; i < arr.size() - 1; i++)
+	for (i = 0; i < arr.size()-1; i++)
 	{ 
 
 		min_idx = i; 
-		for (j = i + 1; j < arr.size(); j++) { 
+		for (j = i + 1; j < arr.size(); j++) 
+		{ 
 			if (arr[j] < arr[min_idx]) 
 				min_idx = j; 
 		} 
