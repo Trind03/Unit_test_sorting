3bd2d240221a25f2ba58d22061a6971f2c1ff6a5
Torstein <141224356+Trind03@users.noreply.github.com>
2024-04-07 22:36:19 +0200

    Moved included sub dir

--- init.cmake
+++ init.cmake
@@ -1,6 +1,9 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_BUILD_PARALLEL_LEVEL 4)
 
+# Add sub directories and add source paths.
+add_subdirectory(googletest)
+
 # Primary directories 
 set(SOURCE_DIR "${CMAKE_SOURCE_DIR}/src")
 set(Algo "${SOURCE_DIR}/algo")
