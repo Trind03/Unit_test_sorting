f6533e8455e3383f5928d2a917f5a0462fc0fa25
Torstein <141224356+Trind03@users.noreply.github.com>
2024-04-15 10:58:08 +0200

    Minor style change

--- src/algo/QuickSort/QuickSort.cpp
+++ src/algo/QuickSort/QuickSort.cpp
@@ -12,7 +12,7 @@ QuickSort::QuickSort(std::array<int,arraySize>&arr)
 int QuickSort::quickSort(std::array<int, arraySize>& arr, int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
-    for (int j = low; j <= high - 1; j++) {
+    for (int j = low; j <= high-1; j++) {
         if (arr[j] < pivot) {
             i++;
             std::swap(arr[i], arr[j]);
